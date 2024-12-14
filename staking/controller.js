75c856c2ae47a82d1058a1d0754237208988af10
Mike Haiduk <x29a100@gmail.com>
2024-12-14 22:02:14 +0100

    update staking and unstaking ui

--- staking/controller.js
+++ staking/controller.js
@@ -1,18 +1,30 @@
 function openStaking(domain, success) {
     trackCall(arguments)
-    showDialog('/mfm-bank/staking/index.html?nocache', success, function ($scope) {
+    let rewardTimer = null
+    showDialog('/mfm-bank/staking/index.html?nocache', function () {
+        if (rewardTimer)
+            clearInterval(rewardTimer)
+        if (success)
+            success()
+    }, function ($scope) {
         $scope.domain = domain
+        $scope.staking_address = 'staking'
 
         $scope.openStake = function () {
             getPin(function (pin) {
                 calcPass(domain, pin, function (pass) {
+                    $scope.in_progress = true
                     postContract("mfm-bank", "stake.php", {
                         domain: domain,
                         amount: $scope.amount,
                         address: wallet.address(),
                         pass: pass,
                     }, function (response) {
-                        showSuccessDialog(str.you_have_staked + " " + $scope.formatAmount(response.staked, domain), $scope.close)
+                        $scope.in_progress = false
+                        showSuccessDialog(str.you_have_staked + " " + $scope.formatAmount(response.staked, domain), init)
+                    }, function (message) {
+                        $scope.in_progress = false
+                        showError(message)
                     })
                 })
             })
@@ -21,12 +33,17 @@ function openStaking(domain, success) {
         $scope.openUnstake = function () {
             getPin(function (pin) {
                 calcPass(domain, pin, function (pass) {
+                    $scope.in_progress = true
                     postContract("mfm-bank", "unstake.php", {
                         domain: domain,
                         address: wallet.address(),
                         pass: pass,
                     }, function (response) {
-                        showSuccessDialog(str.you_have_unstaked + " " + $scope.formatAmount(response.unstaked, domain), $scope.close)
+                        $scope.in_progress = false
+                        showSuccessDialog(str.you_have_unstaked + " " + $scope.formatAmount(response.unstaked, domain), init)
+                    }, function (message) {
+                        $scope.in_progress = false
+                        showError(message)
                     })
                 })
             })
@@ -36,13 +53,19 @@ function openStaking(domain, success) {
             $scope.amount = $scope.token.balance
         }
 
-        function getStakes(){
+        function getStakes() {
             postContract("mfm-bank", "staked.php", {
                 address: wallet.address(),
             }, function (response) {
                 for (const stake_tran of response.staked) {
                     if (stake_tran.domain == domain) {
                         $scope.stake = stake_tran
+                        if (rewardTimer == null)
+                            rewardTimer = setInterval(function () {
+                                let period_percent = Math.min(1, (new Date() / 1000 - stake_tran.time) / (60 * 60 * 24) / $scope.period_days)
+                                $scope.reward = $scope.stake.amount * response.percent / 100 * period_percent
+                                $scope.$apply()
+                            }, 1000)
                         break
                     }
                 }
